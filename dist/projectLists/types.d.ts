212a5fc221b1f7bf9773b82433f67d6ceecf8119
Theeef <realstarkr@gmail.com>
2026-04-15 05:55:42 -0400

    removed extra filter types

--- dist/projectLists/types.d.ts
+++ dist/projectLists/types.d.ts
@@ -1,5 +1,5 @@
 import { ProjectListEntry } from "../projectListEntries/types.js";
-import { Accessibility, ArchitectApproval, CommunityVetted, Project, ProjectStage, ProjectType } from "../projects/types.js";
+import { Project } from "../projects/types.js";
 import { FilterGroup } from "../types/filter.js";
 export interface ProjectList {
     id: number;
@@ -14,16 +14,4 @@ export interface ProjectListWithEntries extends ProjectList {
 export interface ProjectListEntryWithProject extends ProjectListEntry {
     project: Project;
 }
-export type ProjectFilter = {
-    all: ProjectFilter[];
-} | {
-    any: ProjectFilter[];
-} | {
-    field: ProjectFilterField;
-    operator: ProjectFilterOperator;
-    value: ProjectFilterValue;
-};
-export type ProjectFilterValue = ArchitectApproval | CommunityVetted | ProjectStage | Accessibility | ProjectType | string;
-export type ProjectFilterField = "architect_approval" | "community_vetted" | "project_stage" | "accessibility" | "type";
-export type ProjectFilterOperator = "=" | "!=";
 //# sourceMappingURL=types.d.ts.map
