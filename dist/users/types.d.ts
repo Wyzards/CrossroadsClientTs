ecb99e4e60b0e7a2589fb9aaca3d847885d4b86a
Theeef <realstarkr@gmail.com>
2026-04-30 12:07:05 -0400

    added era to CrossroadsUser interface

--- dist/users/types.d.ts
+++ dist/users/types.d.ts
@@ -1,7 +1,9 @@
+import { Era } from "../badges/types.js";
 export interface CrossroadsUser {
     id: number;
     minecraftUuid?: string;
     discordId?: string;
+    era: Era | null;
 }
 export interface CreateCrossroadsUserPayload {
     minecraftUuid?: string;
